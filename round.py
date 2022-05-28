02dcd1ec9a9d8ccd646fb4b7d455adf6325f386b
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-28 17:01:53 +0200

    add Round class

---
+++ round.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel, constr
+
+
+class Round(BaseModel):
+    """
+    The Round() class inherits from the BaseModel() class:it contains the elements of Round
+
+    Args:
+        BaseModel (class): This class contains the attributes and methods of round
+    """
+    name: constr()
+    start_date: datetime = datetime.today()
+    end_date: datetime = None
+    match = List[Match]
