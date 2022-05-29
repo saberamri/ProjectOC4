4268f87064238b21d89f94916737ed43ba8ed914
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-29 09:36:55 +0200

    test to manager load data json

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ from models.player import Player
 
 
 player_manager = Manager(item_type=Player)
-print(player_manager.load_from_jason(path="Jason\players.json"))
+print(player_manager.load_from_json(path="Jason\players.json"))
