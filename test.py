272da2b969513fa5dd5fc85abb37f409e5b5b34d
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-29 10:33:14 +0200

    move the View() and Menu() class from the models folder

--- test.py
+++ test.py
@@ -7,5 +7,6 @@ from models.tournament import Tournament
 # player_manager = Manager(item_type=Player)
 # print(player_manager.load_from_json(path="./Jason/players.json"))
 
-tournament_manager = Manager(item_type=Tournament)
-print(tournament_manager.load_from_json(path="./Jason/tournaments.json"))
+# tournament_manager = Manager(item_type=Tournament)
+# print(tournament_manager.load_from_json(path="./Jason/tournaments.json"))
+
