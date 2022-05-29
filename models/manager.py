47df4b87601215727f05bd42a838eeaadf73eb1d
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-29 09:19:17 +0200

    Add Manager to load data json

---
+++ models/manager.py
@@ -0,0 +1,35 @@
+import json
+
+
+class Manager:
+    """
+    manager: it is a layer between the database and the model:
+    data access layer which is used to serialize and deserialize
+    the data and to:
+    - Create item as player
+    - modify an element as a player
+    - save this element in a database
+
+    """
+
+    def __init__(self, item_type: Any):
+        """
+        main constructors which initializes:
+        - item_type (Any): instance of a class
+        """
+        self.item_type = item_type
+        self.items = {}
+
+    def load_from_jason(self, path: str):
+        """
+        load the jason data of a Player() or Tournament() from a path and
+        store this data in the -items- dictionary of the Player() or
+        Tournament() instance key
+
+        Args:
+            path (str): path of data
+        """
+        with open(path) as json_data:
+            data_dict = json.load(json_data)
+            for item_data in data_dict:
+                self.create(**item_data)
