8a992ac156e7ee94a1db3900df31370d1765d00a
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-29 09:54:57 +0200

    Add View class

---
+++ models/view.py
@@ -0,0 +1,24 @@
+import os
+
+class View:
+    """
+    view contains two attributes title and content and a show class method to display them.
+    """
+    def __init__(self, title: str, content: str):
+        """constructor
+        Args:
+            title (str): capitalized view title
+            content (str): view content: it is a str which allows to present the data
+        """
+        self.title = title.upper()
+        self.content = content
+
+    def show(self):
+        """
+        - display of title and content separated by a line
+        - console cls after each display
+        """
+        print(self.title)
+        print("-" * len(self.title))
+        print(self.content)
+    
